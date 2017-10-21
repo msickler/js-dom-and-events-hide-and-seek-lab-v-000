48654f5da529266a6c6950eab81b4e6d1709efab
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-21 23:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let node = document.querySelector('grand-node')
+  let node = document.getElementById('grand-node')
   let nextNode = node.children[0]
   while (nextNode) {
     node = nextNode
